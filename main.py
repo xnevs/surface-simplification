d5ace1292a04a559d743192fa4a353dd90283778
Sven Cerk <sven.cerk@gmail.com>
2017-05-29 20:38:29 +0200

    fix real_error
    now it holds exactly the same keys as pq

--- main.py
+++ main.py
@@ -44,6 +44,7 @@ with open(filename) as f:
         _,a,b,c = map(int,next(f).split())
         triangles.append((a,b,c))
 
+print('read file')
 
 graph = [set() for _ in points]
 for (a,b,c) in triangles:
@@ -53,6 +54,8 @@ for (a,b,c) in triangles:
     graph[b].add(c)
     graph[c].add(a)
     graph[c].add(b)
+
+print('built graph')
 
 def plane(a,b,c):
     n = np.cross(b-a,c-a)
@@ -66,12 +69,16 @@ def triangle_quadric(a,b,c):
 
 def point_quadric(points,graph,i):
     Q = np.zeros((4,4))
-    for j,k in itertools.combinations(graph[i],2):
-        if k in graph[j]:
+    #for j,k in itertools.combinations(graph[i],2):
+    #    if k in graph[j]:
+    #        Q += triangle_quadric(points[i],points[j],points[k])
+    for j in graph[i]:
+        for k in (graph[i] & graph[j]):
             Q += triangle_quadric(points[i],points[j],points[k])
     return Q
 
 Qs = [point_quadric(points,graph,i) for i in range(len(points))]
+print('after Qs')
 
 def edge_point(points,Qs,i,j):
     Q = Qs[i] + Qs[j]
@@ -83,16 +90,17 @@ def edge_point(points,Qs,i,j):
         c = (points[i] + points[j]) / 2
     c = np.append(c,1)
     error = np.dot(np.dot(c,Q),c)
-    return (-error,(i,j),c[:-1])
+    return (-error,c[:-1])
 
 real_error = dict()
 pq = SortedDict()
 for i in range(len(points)):
     for j in graph[i]:
         if i < j:
-            error,(i,j),c = edge_point(points,Qs,i,j)
+            error,c = edge_point(points,Qs,i,j)
             real_error[n(i,j)] = error
             pq[(error,n(i,j))] = c
+print('after pq initialization')
 
 def is_safe(graph,a,b):
     return len(graph[a] & graph[b]) == 2
@@ -101,20 +109,22 @@ def contract(points,graph,Qs,pq,i,j,c):
     for k in graph[i]:
         try:
             pq.pop((real_error[n(i,k)],(n(i,k))))
+            real_error.pop(n(i,k))
         except:
             pass
     for k in graph[j]:
         try:
             pq.pop((real_error[n(j,k)],(n(j,k))))
+            real_error.pop(n(j,k))
         except:
             pass
     
     graph[i].remove(j)
-    graph[i].update(graph[j] - {i})
+    graph[i].update(graph[j])
     for k in graph[j]:
-        if k != i:
-            graph[k].remove(j)
-            graph[k].add(i)
+        graph[k].discard(j)
+        graph[k].add(i)
+    graph[i].remove(i) # i was added once in 123 and once in 126
     graph[j] = None
 
     points[i] = c
@@ -123,13 +133,17 @@ def contract(points,graph,Qs,pq,i,j,c):
         Qs[k] = point_quadric(points,graph,k)
 
     for k in graph[i]:
-        error,(i,j),c = edge_point(points,Qs,i,k)
-        real_error[n(i,j)] = error;
-        pq[(error,n(i,j))] = c
+        error,c = edge_point(points,Qs,i,k)
+        real_error[n(i,k)] = error;
+        pq[(error,n(i,k))] = c
     
 
 while pq and count > 0:
+    print(len(pq))
+    print(len(real_error))
+    print()
     (error,(i,j)),c = pq.popitem()
+    real_error.pop((i,j))
     if is_safe(graph,i,j):
         contract(points,graph,Qs,pq,i,j,c)
         count -= 1
