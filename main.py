536601d80fdc86a18d9ab78c784a00811b342081
Sven Cerk <sven.cerk@gmail.com>
2017-05-29 23:11:06 +0200

    big changes
    created Surface class
    added ability to output ply

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import itertools
 from sortedcontainers import SortedDict
 
 import numpy as np
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
 
 class EdgePriorityQueue:
     def __init__(self):
@@ -32,53 +30,6 @@ class EdgePriorityQueue:
         except KeyError:
             pass
 
-
-try:
-    filename = sys.argv[1]
-    count = int(sys.argv[2])
-except:
-    print("Sample input for file name:")
-    print("examples/cow.ply")
-    fileName = input("Input file name: ").strip()
-    count = int(input("Input contract length: ").strip())
-
-points = []
-triangles = []
-with open(filename) as f:
-    line = next(f).split()
-    while line[0] != 'element':
-        line = next(f).split()
-    num_points = int(line[2])
-    line = next(f).split()
-    while line[0] != 'element':
-        line = next(f).split()
-    num_triangles = int(line[2])
-    line = next(f).split()
-    while line[0] != 'end_header':
-        line = next(f).split()
-    for i in range(num_points):
-        line = map(float,next(f).split())
-        x = next(line)
-        y = next(line)
-        z = next(line)
-        points.append( np.array((x,y,z)) )
-    for i in range(num_triangles):
-        _,a,b,c = map(int,next(f).split())
-        triangles.append((a,b,c))
-
-print('read file')
-
-graph = [set() for _ in points]
-for (a,b,c) in triangles:
-    graph[a].add(b)
-    graph[a].add(c)
-    graph[b].add(a)
-    graph[b].add(c)
-    graph[c].add(a)
-    graph[c].add(b)
-
-print('built graph')
-
 def plane(a,b,c):
     n = np.cross(b-a,c-a)
     n = n / np.linalg.norm(n)
@@ -89,79 +40,167 @@ def triangle_quadric(a,b,c):
     u = plane(a,b,c)
     return np.outer(u,u)
 
-def point_quadric(points,graph,i):
-    Q = np.zeros((4,4))
-    for j in graph[i]:
-        for k in (graph[i] & graph[j]):
-            Q += triangle_quadric(points[i],points[j],points[k])
-    return Q
+class Surface:
+    def from_ply(self,f):
+        line = next(f).split()
+        while line[0] != 'element':
+            line = next(f).split()
+        num_points = int(line[2])
+        self.n = num_points
+        
+        line = next(f).split()
+        while line[0] != 'element':
+            line = next(f).split()
+        num_triangles = int(line[2])
 
-Qs = [point_quadric(points,graph,i) for i in range(len(points))]
-print('after Qs')
+        line = next(f).split()
+        while line[0] != 'end_header':
+            line = next(f).split()
 
-def edge_point(points,Qs,i,j):
-    Q = Qs[i] + Qs[j]
-    try:
-        c = np.linalg.solve(Q[:-1,:-1],-Q[:-1,-1])
-        ##print("OK")
-    except np.linalg.LinAlgError:  # TODO
-        print("NOT OK")
-        c = (points[i] + points[j]) / 2
-    c = np.append(c,1)
-    error = np.dot(np.dot(c,Q),c)
-    return (-error,c[:-1])
+        self.points = []
+        for _ in range(num_points):
+            line = map(float,next(f).split())
+            x = next(line)
+            y = next(line)
+            z = next(line)
+            self.points.append( np.array((x,y,z)) )
 
+        self.graph = [set() for _ in range(num_points)]
+        self.Qs = [np.zeros((4,4)) for _ in range(num_points)]
+        for _ in range(num_triangles):
+            _,i,j,k = map(int,next(f).split())
+            self.graph[i].update([j,k])
+            self.graph[j].update([i,k])
+            self.graph[k].update([i,j])
+            Q = triangle_quadric(self.points[i],self.points[j],self.points[k])
+            self.Qs[i] += Q
+            self.Qs[j] += Q
+            self.Qs[k] += Q
 
-pq = EdgePriorityQueue()
-for i in range(len(points)):
-    for j in graph[i]:
-        if i < j:
-            error,c = edge_point(points,Qs,i,j)
-            pq.push(error,(i,j),c)
-print('after pq initialization')
+    def ply(self):
+        points,triangles = self.points_and_triangles()
+        lines = []
+        lines.append('ply')
+        lines.append('format ascii 1.0')
+        lines.append('element vertex {}'.format(len(points)))
+        lines.append('property float x')
+        lines.append('property float y')
+        lines.append('property float z')
+        lines.append('element face {}'.format(len(triangles)))
+        lines.append('property list uchar uint vertex_indices')
+        lines.append('end_header')
+        for p in points:
+            lines.append(' '.join(map(str,p)))
+        for t in triangles:
+            lines.append('3 ' + ' '.join(map(str,t)))
+        return '\n'.join(lines)
 
-def is_safe(graph,a,b):
-    return len(graph[a] & graph[b]) == 2
+    def from_points_and_triangles(self,points,triangles):
+        self.n = len(points)
+        self.points = points.copy()
+        self.graph = [set() for _ in points]
+        self.Qs = [np.zeros((4,4)) for _ in points]
+        for (i,j,k) in triangles:
+            self.graph[i].update([j,k])
+            self.graph[j].update([i,k])
+            self.graph[k].update([i,j])
+            Q = triangle_quadric(self.points[i],self.points[j],self.points[k])
+            self.Qs[i] += Q
+            self.Qs[j] += Q
+            self.Qs[k] += Q
 
-def contract(points,graph,Qs,pq,i,j,c):
-    for k in graph[i]:
+    def points_and_triangles(self):
+        points = []
+        idxs = dict()
+        count = 0
+        for i in range(self.n):
+            if self.points[i] is not None:
+                points.append(self.points[i])
+                idxs[i] = count
+                count += 1
+        triangles = [(idxs[i],idxs[j],idxs[k])
+                        for i in idxs.keys()
+                            if self.graph[i]
+                                for j in self.graph[i]
+                                    if i < j
+                                        for k in (self.graph[i] & self.graph[j])
+                                            if j < k]
+        return points,triangles
+
+    def point_quadric(self,i):
+        Q = np.zeros((4,4))
+        for j in self.graph[i]:
+            for k in (self.graph[i] & self.graph[j]):
+                Q += triangle_quadric(self.points[i],self.points[j],self.points[k])
+        return Q
+
+    def edge_point(self,i,j):
+        Q = self.Qs[i] + self.Qs[j]
+        try:
+            c = np.linalg.solve(Q[:-1,:-1],-Q[:-1,-1])
+        except np.linalg.LinAlgError:  # TODO
+            print("NOT OK")
+            c = (self.points[i] + self.points[j]) / 2
+        c = np.append(c,1)
+        error = np.dot(np.dot(c,Q),c)
+        return (-error,c[:-1])
+
+    def is_safe(self,i,j):
+        return len(self.graph[i] & self.graph[j]) == 2
+
+    def contract(self,i,j,c):
+        self.points[i] = c
+        self.points[j] = None
+        
+        self.graph[i].remove(j)
+        self.graph[i].update(self.graph[j])
+        for k in self.graph[j]:
+            self.graph[k].discard(j)
+            self.graph[k].add(i)
+        self.graph[i].remove(i) # i was added once before the for loop and once in it
+        self.graph[j] = None
+
+        self.Qs[i] = self.point_quadric(i)
+        for k in self.graph[i]:
+            self.Qs[k] = self.point_quadric(k)
+        self.Qs[j] = None
+
+def contract(surface,pq,i,j,c):
+    for k in surface.graph[i]:
         pq.discard((i,k))
-    for k in graph[j]:
+    for k in surface.graph[j]:
         pq.discard((j,k))
-    
-    graph[i].remove(j)
-    graph[i].update(graph[j])
-    for k in graph[j]:
-        graph[k].discard(j)
-        graph[k].add(i)
-    graph[i].remove(i) # i was added once in 123 and once in 126
-    graph[j] = None
-
-    points[i] = c
-    Qs[i] = point_quadric(points,graph,i)
-    for k in graph[i]:
-        Qs[k] = point_quadric(points,graph,k)
-
-    for k in graph[i]:
-        error,c = edge_point(points,Qs,i,k)
+    surface.contract(i,j,c)
+    for k in surface.graph[i]:
+        error,c = surface.edge_point(i,k)
         pq.push(error,(i,k),c)
-    
+        
 
-while pq and count > 0:
-    (i,j),c = pq.pop()
-    if is_safe(graph,i,j):
-        contract(points,graph,Qs,pq,i,j,c)
-        count -= 1
+if __name__ == '__main__':
+    try:
+        filename = sys.argv[1]
+        count = int(sys.argv[2])
+    except:
+        print("Sample input for file name:")
+        print("examples/cow.ply")
+        filename = input("Input file name: ").strip()
+        count = int(input("Input contract length: ").strip())
 
-Tri = [(i,j,k) for i in range(len(graph)) if graph[i] for j in graph[i] if graph[j] and i<j for k in (graph[i] & graph[j]) if graph[k] and j<k]
+    surface = Surface()
+    with open(filename) as f:
+        surface.from_ply(f)
 
-P = np.array(points)
-for i in range(len(graph)):
-    if graph[i] is None:
-        P[i,:] = np.array([0,0,0])
+    pq = EdgePriorityQueue()
+    for i in range(surface.n):
+        for j in surface.graph[i]:
+            if i < j:
+                error,c = surface.edge_point(i,j)
+                pq.push(error,(i,j),c)
+        
+    while pq and count > 0:
+        (i,j),c = pq.pop()
+        if surface.is_safe(i,j):
+            contract(surface,pq,i,j,c)
+            count -= 1
 
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-
-ax.plot_trisurf(P[:,0],P[:,1],P[:,2],triangles=Tri)
-plt.show()
+    print(surface.ply())
